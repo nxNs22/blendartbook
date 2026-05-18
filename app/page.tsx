3307f1d8a4ee36556af982c1ec68247a829c46be
Yakup Akyniyazov <yakup.akinyaz@gmail.com>
2026-05-18 22:59:31 +0300

    Kategoriler silindi V2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import FeaturesBar from "./components/FeaturesBar";
 import BestsellersSection from "./components/BestsellersSection";
-import CategoriesSection from "./components/CategoriesSection";
 import PromoBanner from "./components/PromoBanner";
 import Footer from "./components/Footer";
 import NewArrivals from "./components/NewArrivals";
